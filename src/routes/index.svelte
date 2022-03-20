1f88a8709fa305e071b4dbaabeb8166066b11635
SB <dpoakaspine@googlemail.com>
2022-03-20 19:08:22 +0100

    new

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
   import { base } from '$app/paths';
 
   export async function load({ fetch }) {
-    const posts = await fetch(`${base}/posts/index.json`)
+    const posts = await fetch(`${base}/index.json`)
         .then((r) => r.json());
     return {
       props: { posts }
@@ -28,3 +28,24 @@
     </a>
   {/each}
 </div>
+
+<style lang="scss">
+
+  h1 {
+    margin-bottom: 0;
+  }
+
+  h2.title {
+    margin-top: 32px;
+    margin-bottom: 0;
+
+    &:hover {
+      color: #40b3ff;
+    }
+  }
+
+  p {
+    color: #555;
+    margin: 0;
+  }
+</style>
