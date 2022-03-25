022d6ce2c74cb0f75e4968bfc6090d1125039806
SB <dpoakaspine@googlemail.com>
2022-03-25 18:02:31 +0100

    Update {MarkdownRemark.frontmatter__slug}.js

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,5 +1,10 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+import Seo from '../components/seo'
+
 
 export default function Template({
   data, // this prop will be injected by the GraphQL query below.
@@ -9,6 +14,7 @@ export default function Template({
   return (
 
     <Layout>
+    <Seo title="frontmatter.title}" keywords={[`gatsby2`, `application2`, `react2`]} />
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
